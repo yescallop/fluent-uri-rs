e9a980d33856bf2c5efbb53b33547be7bedd3653
yescallop <yescallop@gmail.com>
2024-01-18 23:38:17 +0800

    Clean up for refactoring

--- fuzz/fuzz_targets/against_iref.rs
+++
@@ -1,8 +0,0 @@
-#![no_main]
-use libfuzzer_sys::fuzz_target;
-
-fuzz_target!(|data: &str| {
-    if fluent_uri::Uri::parse(data).is_ok() {
-        assert!(iref::IriRef::from_str(data).is_ok());
-    }
-});
--- fuzz/fuzz_targets/dec.rs
+++
@@ -1,7 +0,0 @@
-#![no_main]
-use fluent_uri::enc::*;
-use libfuzzer_sys::fuzz_target;
-
-fuzz_target!(|data: &[u8]| {
-    let _ = decode(data);
-});
