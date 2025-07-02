1106fbcab0d8189c5a0031046de47b4b31cf661e
Scallop Ye <yescallop@gmail.com>
2025-07-02 09:15:04 +0800

    Make sure fuzzing works

--- fuzz/uriparser-sys/src/lib.rs
+++ fuzz/uriparser-sys/src/lib.rs
@@ -1,6 +1,6 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
-#![allow(improper_ctypes)]
+#![allow(unnecessary_transmutes)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
