79bfe99b938cd1bb62d5c91891cf2bb05af9e34f
yescallop <yescallop@gmail.com>
2025-11-03 15:24:30 +0800

    Fix and update fuzz targets

--- fuzz/fuzz_targets/enc_dec.rs
+++ fuzz/fuzz_targets/enc_dec.rs
@@ -4,6 +4,6 @@ use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     let mut buf = EString::<Query>::new();
-    buf.encode::<Query>(data);
+    buf.encode_bytes::<Query>(data);
     assert_eq!(data, &*buf.decode().to_bytes());
 });
