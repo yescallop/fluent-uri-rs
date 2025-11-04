039a4740f20d9d8f0f8b966752881022f907e774
yescallop <yescallop@gmail.com>
2025-11-04 23:35:58 +0800

    Update fuzz target

--- fuzz/fuzz_targets/enc_dec.rs
+++ fuzz/fuzz_targets/enc_dec.rs
@@ -1,9 +1,23 @@
 #![no_main]
-use fluent_uri::pct_enc::{encoder::Query, EString};
+use fluent_uri::pct_enc::{encoder::Query, EStr, EString};
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     let mut buf = EString::<Query>::new();
-    buf.encode_bytes::<Query>(data);
+    let mut lossy = String::new();
+
+    for chunk in data.utf8_chunks() {
+        buf.encode_str::<Query>(chunk.valid());
+        lossy.push_str(chunk.valid());
+
+        for &x in chunk.invalid() {
+            buf.push_estr(EStr::encode_byte(x));
+        }
+        if !chunk.invalid().is_empty() {
+            lossy.push(char::REPLACEMENT_CHARACTER);
+        }
+    }
+
     assert_eq!(data, &*buf.decode().to_bytes());
+    assert_eq!(lossy, buf.decode().to_string_lossy());
 });
