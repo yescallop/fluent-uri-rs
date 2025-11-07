43b7880ae255ec73138e3036555e142cc1c969c1
yescallop <yescallop@gmail.com>
2025-11-07 22:29:35 +0800

    Update fuzz targets

--- fuzz/fuzz_targets/normalize_iri_against_iri_string.rs
+++ fuzz/fuzz_targets/normalize_iri_against_iri_string.rs
@@ -43,8 +43,12 @@ fuzz_target!(|data: &str| {
         return;
     }
 
-    if let Some(auth) = r1.authority() {
-        match auth.host_parsed() {
+    if let Some(auth1) = r1.authority() {
+        let auth2 = r2.authority_components().unwrap();
+        assert_eq!(auth1.userinfo().map(|s| s.as_str()), auth2.userinfo());
+        assert_eq!(auth1.port().map(|s| s.as_str()), auth2.port());
+
+        match auth1.host_parsed() {
             Host::RegName(name1) => {
                 let name2 = r2.authority_components().unwrap().host();
                 if name1.as_str().eq_ignore_ascii_case(name2) {
